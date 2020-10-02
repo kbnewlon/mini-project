4aa09163e7a7d85fbecb898b961c4a75ddd753bd
Kayla Newlon <kbnewlon@hotmail.com>
2020-10-01 20:30:52 -0700

    completed

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-function htmlTemplate(userAnswer) {
-    `<!doctype html>
-    <html lang="en">
-      <head>
-         <!-- Required meta tags -->
-<meta charset="utf-8">
-         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-        <title>Hello, world!</title>
-       </head>
-       <body>
-         <div class="jumbotron">
-             <h4 class="display-4">Name: ${userAnswer.userName}</h4>
-             <h4 class="display-4">Location: ${userAnswer.location}</h4>
-             <h4 class="display-4">Bio: ${userAnswer.bio}</h4>
-            <h4 class="display-4">LinkedIn URL: ${userAnswer.linkedinUrl}</h4>
-             <h4 class="display-4">Github URL: ${userAnswer.githubUrl}</h4>
-           
-           </div>
-    
-         <!-- Optional JavaScript -->
-         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-       </body>
-    </html>`
-}
+
 
 inquirer.prompt([
     {
@@ -63,6 +33,48 @@ inquirer.prompt([
 
 ]).then(function (data) {
     console.log(data)
-    
-})
+    const MyHtmlTemplate = htmlTemplate(data)
 
+    fs.writeFile("profile.html", MyHtmlTemplate, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("success!")
+        }
+
+    });
+
+
+
+    function htmlTemplate(userAnswer) {
+        return `<!doctype html>
+    <html lang="en">
+      <head>
+         <!-- Required meta tags -->
+    <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+        <title>Hello, world!</title>
+       </head>
+       <body>
+         <div class="jumbotron">
+             <h4 class="display-4">Name: ${userAnswer.userName}</h4>
+             <h4 class="display-4">Location: ${userAnswer.location}</h4>
+             <h4 class="display-4">Bio: ${userAnswer.bio}</h4>
+            <h4 class="display-4">LinkedIn URL: ${userAnswer.linkedinUrl}</h4>
+             <h4 class="display-4">Github URL: ${userAnswer.githubUrl}</h4>
+           
+           </div>
+    
+         <!-- Optional JavaScript -->
+         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+       </body>
+    </html>`
+    }
+});
